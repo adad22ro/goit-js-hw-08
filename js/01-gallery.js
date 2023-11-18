4d1631f912fc68d7996f58e8a1a1750ea1f72604
adad22ro <gabirusu2000@gmail.com>
2023-11-18 14:41:42 +0200

    Final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+console.log(galleryItems);
 
+const listEl = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const listItemEl = document.createElement('li');
+  listItemEl.classList.add('galleryitem');
+  listItemEl.innerHTML = `<a class='gallerylink' href='${item.original}'>
+        <img class='gallery__image'
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'/>
+    </a>`;
+
+  listEl.append(listItemEl);
+});
+
+listEl.addEventListener('click', openImageInLightbox);
+
+function openImageInLightbox(event) {
+  const clickedOn = event.target;
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  event.preventDefault();
+
+  const lightbox = new SimpleLightbox('.gallery a', {});
+}
 console.log(galleryItems);
